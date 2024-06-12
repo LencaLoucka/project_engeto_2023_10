5fec0ff9965998a4599258994bea6463c84203f1
LencaLoucka <149104911+LencaLoucka@users.noreply.github.com>
2024-06-12 13:21:08 +0200

    Update script5.sql

--- script5.sql
+++ script5.sql
@@ -12,7 +12,7 @@ FROM (
 			round(avg(food_value), 2) AS average_food_value
 		FROM t_lenka_loucka_project_sql_primary_final
 		WHERE food_category IS NOT NULL 
-		GROUP BY food_category, payroll_year)av
+		GROUP BY food_category, payroll_year) av
 JOIN (
 		SELECT
 			food_category,
@@ -20,7 +20,7 @@ JOIN (
 			round(avg(food_value), 2) AS average_food_value
 		FROM t_lenka_loucka_project_sql_primary_final
 		WHERE food_category IS NOT NULL 
-		GROUP BY food_category, payroll_year)av2
+		GROUP BY food_category, payroll_year) av2
 	ON av.food_category = av2.food_category
 	AND av.payroll_year = av2.payroll_year + 1
 HAVING growth > 0
