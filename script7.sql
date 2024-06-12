9f753bb379c8578d6f328cc5b135571f83020dcd
LencaLoucka <149104911+LencaLoucka@users.noreply.github.com>
2024-06-12 13:21:30 +0200

    Update script7.sql

--- script7.sql
+++ script7.sql
@@ -10,7 +10,7 @@ FROM (
 		round(avg(avg_value), 2) AS 'avg_value2',
 		round (avg(food_value), 2) AS 'food_value2'
 	FROM t_lenka_loucka_project_sql_secondary_final
-	GROUP BY payroll_year, GDP)av
+	GROUP BY payroll_year, GDP) av
 JOIN (
 	SELECT 
 		payroll_year,
@@ -18,7 +18,7 @@ JOIN (
 		round(avg(avg_value), 2) AS 'avg_value2',
 		round (avg(food_value), 2) AS 'food_value2'
 	FROM t_lenka_loucka_project_sql_secondary_final
-	GROUP BY payroll_year,GDP)av2
+	GROUP BY payroll_year,GDP) av2
 ON av.payroll_year = av2.payroll_year + 1
 GROUP BY payroll_year
 HAVING food_value2 IS NOT NULL;
